e10f6e22e9f23b98e5dcaac2b9938967d6cd7cfe
Yuji Sugiura <y.sugiura.0316@gmail.com>
2022-03-14 22:53:35 +0900

    Avoid format! macro

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,5 +2,8 @@ use worker::*;
 
 #[event(fetch)]
 pub async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Response> {
-    Response::ok(format!("Hello worker! at {}", req.url()?))
+    // See https://rustwasm.github.io/book/reference/code-size.html#avoid-string-formatting
+    // let res = format!("Hello worker! at {}", req.url()?);
+    let res = String::from("Hello worker! at ") + &req.url()?.to_string();
+    Response::ok(res)
 }
