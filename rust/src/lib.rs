1d12bc52bbda9ca21484920c0ac8b6737cd58a91
Yuji Sugiura <y.sugiura.0316@gmail.com>
2022-03-15 08:12:21 +0900

    Remove to_string() 340Kb > 337Kb

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,9 +1,6 @@
 use worker::*;
 
 #[event(fetch)]
-pub async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Response> {
-    // See https://rustwasm.github.io/book/reference/code-size.html#avoid-string-formatting
-    // let res = format!("Hello worker! at {}", req.url()?);
-    let res = String::from("Hello worker! at ") + &req.url()?.to_string();
-    Response::ok(res)
+pub async fn main(_req: Request, _env: Env, _ctx: worker::Context) -> Result<Response> {
+    Response::ok("Hello worker!")
 }
